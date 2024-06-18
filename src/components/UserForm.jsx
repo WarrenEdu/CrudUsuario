d00df4a025177de3b2869583791a623684c4d0f4
Vicent-Duarte <vicentduarte70@gmail.com>
2024-06-17 22:55:42 -0400

    arreglando estilo

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,79 +1,90 @@
+import { useForm } from "react-hook-form";
+import "./styles/UserForm.css";
 import React, { useEffect } from 'react';
-import { useForm } from 'react-hook-form';
-import './styles/UserForm.css';
+import { Toaster, toast } from "sonner";
 
-const UserForm = ({createUser, upDateUser, editUser, setUpDateUser, isOpen, setIsOpen}) => {
-    /**first_name, last_name, email, password, birthday */
 
-    const {handleSubmit, register, reset } = useForm();
+const UserForm = ({ createUsers, updateUser, editUsers, setUpdateUser, isOpen, setIsOpen }) => {
+
+    const { handleSubmit, register, reset } = useForm();
 
     useEffect(() => {
-        if (upDateUser) {
-            reset(upDateUser);
+        if (updateUser) {
+            reset(updateUser);
             setIsOpen(true);
         }
-    }, [upDateUser]);
-    
+    }, [updateUser]);
 
     const submit = data => {
-        if (upDateUser) {
-            editUser('users', data, upDateUser.id);
-            setUpDateUser();
+        if (updateUser) {
+            editUsers('users', data, updateUser.id);
+            setUpdateUser();
         } else {
-            createUser('users', data);
+            createUsers('users', data);
         }
-        setIsOpen(false);
+        setIsOpen(false)
         reset({
-            first_name: '',
-            last_name: '',
-            email: '',
-            password: '',
-            birthday: '',
-        });
+            first_name: "",
+            last_name: "",
+            email: "",
+            password: "",
+            birthday: "",
+            avatar: "",
+            image_url: "",
+        })
+        toast("Created user", {
+            description: `${data.first_name} ${data.last_name} was created`
+        })
     }
 
     const handleClose = () => {
-        setIsOpen(false);
-        setUpDateUser();
+        setIsOpen(false)
+        setUpdateUser();
         reset({
-            first_name: '',
-            last_name: '',
-            email: '',
-            password: '',
-            birthday: '',
-        });
+            first_name: "",
+            last_name: "",
+            email: "",
+            password: "",
+            birthday: "",
+            avatar: "",
+            image_url: "",
+        })
     }
 
-  return (
-    <div className={`form__back ${isOpen && 'active' }`}>
-        <form className='form' onSubmit={handleSubmit(submit)}>
-
-            <button className='form__close' onClick={handleClose}>X</button>
-            <h2 className='form__title'>User Form</h2>
-            <div className='form__item'>
-                <label htmlFor="first_name">First name</label>
-                <input {...register('first_name')} id='first_name' type="text" />
-            </div>
-            <div className='form__item'>
-                <label htmlFor="last_name">Last name</label>
-                <input {...register('last_name')} id='last_name' type="text" />
-            </div>
-            <div className='form__item'>
-                <label htmlFor="email">Email</label>
-                <input {...register('email')} id='email' type="text" />
-            </div>
-            <div className='form__item'>
-                <label htmlFor="password">Password</label>
-                <input {...register('password')} id='password' type="password" />
-            </div>
-            <div className='form__item'>
-                <label htmlFor="birthday">Birthday</label>
-                <input {...register('birthday')} id='birthday' type="date" />
-            </div>
-            <button className='form__btn'>Submit</button>
-        </form>
-    </div>
-  )
+    return (
+        <div className={`form__back ${isOpen && `active`}`}>
+            <form className="form" onSubmit={handleSubmit(submit)}>
+                <button onClick={handleClose} type="button" className="form__close"><ion-icon name="close"></ion-icon></button>
+                <h2 className="form__title">Edit user</h2>
+                <div className="form__item">
+                    <label htmlFor="first_name">First name</label>
+                    <input {...register('first_name')} id="first_name" type="text" />
+                </div>
+                <div className="form__item">
+                    <label htmlFor="last_name">Last name</label>
+                    <input {...register('last_name')} id="last_name" type="text" />
+                </div>
+                <div className="form__item">
+                    <label htmlFor="email">E-mail</label>
+                    <input {...register('email')} id="email" type="text" />
+                </div>
+                <div className="form__item">
+                    <label htmlFor="password">Password</label>
+                    <input {...register('password')} id="password" type="password" />
+                </div>
+                <div className="form__item">
+                    <label htmlFor="birthday">Birthday</label>
+                    <input {...register('birthday')} id="birthday" type="date" />
+                </div>
+                <div className="form__item">
+                    <label htmlFor="image_url">Avatar</label>
+                    <input {...register('image_url')} id="image_url" type="text" />
+                </div>
+                <button className="form__btn">Send</button>
+            </form>
+            <Toaster />
+        </div>
+    )
 }
 
 export default UserForm;
--- src/hooks/useCrud.js
+++ src/hooks/useCrud.js
@@ -1,51 +1,62 @@
-import React, { useState } from 'react';
-import axios from "axios";
+import axios from 'axios';
+import { useState } from 'react';
 
 const useCrud = (base) => {
     const [apiData, setApiData] = useState();
-    const [isLoading, setIsLoading] = useState(true);
-    const [hasError, setHasError] = useState(false);
 
- //leer
-    const getApi = (path='users') => {
-    const url = `${base}${path}/`;
+    // LEER EL ENDPOINT
+    const getApi = (path) => {
+        const url = `${base}${path}/`;
         axios.get(url)
-         .then(res => setApiData(res.data))
-         .catch(err => console.log(err));
-}
-//crear
-const postApi = (path, data) => {
-    const url = `${base}${path}/`
-     axios.post(url, data)
-        .then(res => {
-            setApiData([...apiData, res.data]);
-            console.log(res.data)})
-        .catch(err => console.log(err));
-}
+            .then(res => setApiData(res.data))
+            .catch(err => console.log(err))
+    }
 
-//eliminar
-const deleteApi = (path, id) => {
-    const url = `${base}${path}/${id}/`;
-    axios.delete(url)
-    .then(()=> {
-        setApiData(apiData.filter((user) => user.id!==id));
-        console.log('Delete Success')
-    })
-    .catch(err => console.log(err));
-}
+    //! CREAR USUARIO
+    const postApi = (path, data) => {
+        const url = `${base}${path}/`
+        axios.post(url, data)
+            .then(res => {
+                setApiData([...apiData, res.data]);
 
-// actualizar
-const patchApi = (path, data, id) => {
-    const url = `${base}${path}/${id}/`;
-    axios.patch(url, data)
-    .then(res => {
-        setApiData(apiData.map((user) => user.id===id ? res.data : user));
-        console.log(res.data)
-    })
-    .catch(err => console.log(err));
-}
+            })
+            .catch(err => console.log(err))
+    }
 
-return [apiData, getApi, postApi, deleteApi, patchApi]
+    //? BORRAR USUARIO
+    const deleteApi = (path, id) => {
+        const url = `${base}${path}/${id}`;
+        axios.delete(url)
+            .then(() => {
+                setApiData(apiData.filter(user => user.id !== id))
+            })
+            .catch(err => console.log(err))
+    }
+
+    //* MODIFICAR USUARIO
+    const patchApi = (path, data, id) => {
+        const url = `${base}${path}/${id}/`;
+        axios.patch(url, data)
+            .then(res => {
+                setApiData(apiData.map(user => user.id === id ? data : user));
+                console.log(res.data);
+            })
+            .catch(err => console.log(err))
+    }
+
+    //TODO CUIDADO ELIMINAR TODO
+    const deleteAllApi = (path) => {
+        Promise.all(
+            apiData.map(user =>
+                axios.delete(`${base}${path}/${user.id}/`)
+            )
+        ).then(() => {
+            setApiData([]); // Vaciar la lista de usuarios
+        }).catch(err => console.log(err));
+    }
+
+
+    return [apiData, getApi, postApi, deleteApi, patchApi, deleteAllApi];
 }
 
 export default useCrud;
